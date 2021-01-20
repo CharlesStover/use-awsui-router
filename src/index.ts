0d3cb4eeaa8f3eb3e6f130f336423e4ad758c860
Charles Stover <github@charlesstover.com>
2021-01-19 22:17:41 -0800

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export default function App(): void {
-  return;
-}
+export { default as useBreadcrumbs } from './hooks/use-breadcrumbs';
+export { default as useSideNavigation } from './hooks/use-side-navigation';
+export { default as useTabs } from './hooks/use-tabs';
